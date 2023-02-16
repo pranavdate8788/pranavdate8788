3d7c3d1da1c8790f875e8f515973f39d6bd555fd
aniket-tote <anikettote2001@gmail.com>
2023-02-16 20:15:40 +0530

    made nav responsive

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 function SendMail() {
-    event.preventDefault();
-    var params = {
-      from_name: document.getElementById("name").value,
-      email_id: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-    console.log(params);
-    emailjs.sendForm("service_r6ebwfl", "template_9np62vi", params).then(
-      function () {
-        console.log("SUCCESS!");
-      },
-      function (error) {
-        console.log("FAILED...", error);
+  event.preventDefault();
+  var params = {
+    from_name: document.getElementById("name").value,
+    email_id: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  console.log(params);
+  emailjs.sendForm("service_r6ebwfl", "template_9np62vi", params).then(
+    function () {
+      console.log("SUCCESS!");
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+  alert("Thank you..! See you Soon....");
+  document.getElementById("contact-form").reset();
+}
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    // Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
       }
-    );
-    alert("Thank you..! See you Soon....");
-    document.getElementById("contact-form").reset();
-  }
-  
-  
+    });
+
+    // Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();

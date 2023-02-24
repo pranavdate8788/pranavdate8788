1d872d9d9c97d80a2dc85434f3138322b8593707
pranav date <pranavdate908@gmail.com>
2023-02-24 12:00:26 +0530

    links added to footer and whatsapp messaging added

--- script.js
+++ script.js
@@ -18,29 +18,10 @@ function SendMail() {
   document.getElementById("contact-form").reset();
 }
 
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-
-  burger.addEventListener("click", () => {
-    // Toggle Nav
-    nav.classList.toggle("nav-active");
-
-    // Animate Links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.5
-        }s`;
-      }
-    });
-
-    // Burger Animation
-    burger.classList.toggle("toggle");
-  });
-};
-
-navSlide();
+ 
+function sendMessage() {
+  var message = encodeURIComponent("Hello, this is a test message.");
+  var phoneNumber = "+918788046270"; // replace with the phone number you want to message
+  var url = "https://wa.me/" + phoneNumber + "/?text=" + message;
+  window.open(url);
+}
